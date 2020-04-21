a022d777d4020bf65285fc578b1f17db5f644b04
Chris J W Walker <8091191+chrisjwwalker@users.noreply.github.com>
2020-04-21 21:46:42 +0100

    Face recognition in cv2 frame

--- presencedetector/app.py
+++ presencedetector/app.py
@@ -12,10 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+
 # from presencedetector.ml.objectdetection.static_object_detector import StaticObjectDetector
 # from presencedetector.ml.objectdetection.video_object_detector import VideoObjectDetector
 from presencedetector.ml.facerecognition.face_recogniser import FaceRecogniser
 from presencedetector.config.config_loader import ConfigLoader
+from imutils.video import VideoStream
 import os
 import cv2
 
@@ -30,16 +32,4 @@ class App:
         if not self.face_rec.is_model_generated():
             self.face_rec.load_dataset()
 
-        test_img = self.config['face-recognition.test-img']
-        self.face_rec.run_inference_on(test_img)
-
-        # if self.config['mode.static']:
-        #     obj_detector = StaticObjectDetector(self.exec_path)
-        #     obj_detector.run_inference_on(
-        #         input_path=os.path.join(self.exec_path, self.config['obj-detector.input-path']),
-        #         output_path=os.path.join(self.exec_path, self.config['obj-detector.output-path.static'])
-        #     )
-        # else:
-        #     video_obj_detector = VideoObjectDetector(self.exec_path)
-        #     video_input = cv2.VideoCapture(0)
-        #     video_obj_detector.run_inference_on(camera=video_input)
+        self.face_rec.run_inference_on_video(VideoStream(src=0).start())
--- presencedetector/ml/facerecognition/face_recogniser.py
+++ presencedetector/ml/facerecognition/face_recogniser.py
@@ -15,8 +15,9 @@
 from presencedetector.config.config_loader import ConfigLoader
 from imutils import paths
 import face_recognition
-import argparse
+import imutils
 import pickle
+import time
 import cv2
 import os
 
@@ -35,6 +36,7 @@ class FaceRecogniser:
         except FileNotFoundError:
             print('No model.lock detected, generating model based on face rec dataset')
             f = open('model.lock', 'x')
+            f.close()
             return False
 
 
@@ -93,3 +95,62 @@ class FaceRecogniser:
             # cv2.waitKey(0)
 
             print(names)
+
+    def run_inference_on_video(self, video_input):
+        print('Loading encodings')
+        data = pickle.loads(open(self.conf['encodings-file'], 'rb').read())
+
+        writer = None
+        time.sleep(2.0)
+
+        while True:
+            frame = video_input.read()
+
+            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            rgb = imutils.resize(frame, width=750)
+            r = frame.shape[1] / float(rgb.shape[1])
+
+            boxes = face_recognition.face_locations(rgb, model=self.conf['mode'])
+            encodings = face_recognition.face_encodings(rgb, boxes)
+            names = []
+
+            for encoding in encodings:
+                matches = face_recognition.compare_faces(data['encodings'], encoding)
+                name = 'Unknown'
+
+                if True in matches:
+                    matched_idxs = [i for (i, b) in enumerate(matches) if b]
+                    counts = {}
+
+                    for i in matched_idxs:
+                        name = data['names'][i]
+                        counts[name] = counts.get(name, 0) + 1
+
+                    name = max(counts, key=counts.get)
+
+                names.append(name)
+
+                for ((top, right, bottom, left), name) in zip(boxes, names):
+                    # rescale the face coordinates
+                    top = int(top * r)
+                    right = int(right * r)
+                    bottom = int(bottom * r)
+                    left = int(left * r)
+                    # draw the predicted face name on the image
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                    y = top - 15 if top - 15 > 15 else top + 15
+                    cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+                if writer is not None:
+                    writer.write(frame)
+
+                cv2.imshow("Frame", frame)
+                key = cv2.waitKey(1) & 0xFF
+                # if the `q` key was pressed, break from the loop
+                if key == ord("q"):
+                    break
+
+                # cv2.destroyAllWindows()
+                # check to see if the video writer point needs to be released
+                if writer is not None:
+                    writer.release()
